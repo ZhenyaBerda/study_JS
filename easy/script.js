f15dfae85176d6c73ebd38b970ac11a2fc7f0ea8
unknown <bev030997@gmail.com>
2020-01-24 16:00:38 +0300

    easy task: code modified

--- easy/script.js
+++ easy/script.js
@@ -1,28 +1,24 @@
-let money = 60000,
-income = '10000',
+const money = 60000,
+mission = 100000,
+period = 4;
+let income = 'стипендия',
 addExpenses = 'Коммуналка, Электроэнергия, Интернет, Мобильная связь',
 deposit = true,
-mission = 100000,
-period = 4,
-budgetDay; // новая переменная
+budgetDay = money / 30; // новая переменная
 
 // Вывод типов данных
-console.log('Тип данных money: '+ typeof money);
-console.log('Тип данных income: '+ typeof income);
-console.log('Тип данных deposit: '+ typeof deposit);
+console.log(`Тип данных money: ${typeof money}`);
+console.log(`Тип данных income: ${typeof income}`);
+console.log(`Тип данных deposit: ${typeof deposit}`);
 
 // Вывод длина строки addExpenses
-console.log('Длина строки addExpenses: ' + addExpenses.length);
+console.log(`Длина строки addExpenses: ${addExpenses.length}`);
 
 // Вывод периода и цели
-console.log('Период равен ' + period + ' месяца(ев) \nЦель заработать ' + mission + ' рублей');
-
-// Приведение строки addExpenses
-addExpenses = addExpenses.toLowerCase();
+console.log(`Период равен ${period} месяца(ев) \nЦель заработать ${mission} рублей`);
 
 // Разбивка строки на массив
-console.log(addExpenses.split(', '));
+console.log(addExpenses.toLowerCase().split(', '));
 
 // Дневной бюджет
-budgetDay = money / 30;
-console.log('Дневной бюджет ' + budgetDay + ' рублей');
+console.log(`Дневной бюджет ${budgetDay} рублей`);
