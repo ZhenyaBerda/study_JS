67c715ee391fa74fbf4b51bf84d0b87d761e3cc6
unknown <bev030997@gmail.com>
2020-01-24 16:50:48 +0300

    hard task: fix variable name

--- hard/script.js
+++ hard/script.js
@@ -1,21 +1,21 @@
 // Усложненное задание
 
-const num = 266219;
-let numArr = num.toString().split(''); //массив из цифр числа num
+const num = 266219,
+numArr = num.toString().split(''); //массив из цифр числа num
 
 
 // Вывод массива
 console.log(numArr);
 
-let result = numArr.reduce(function(mult, current) {
+const multNum = numArr.reduce(function(mult, current) {
     return mult * current;
 }, 1);
 
 // Вывод в консоль результата перемножения цифр
-console.log(`Произведение цифр числа 266219: ${result}`);
+console.log(`Произведение цифр числа 266219: ${multNum}`);
 
 // Возведение в степень
-console.log(`Возведение в степень: ${result**3}`);
+console.log(`Возведение в степень: ${multNum**3}`);
 
 // Вывод первых 2х цифр
-console.log(`Первые две цифры: ${result.toString().substr(0, 2)}`);
+console.log(`Первые две цифры: ${multNum.toString().substr(0, 2)}`);
