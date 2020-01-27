74e983305e6a8e79eced035220bac73ae4f02b44
unknown <bev030997@gmail.com>
2020-01-27 21:46:08 +0300

    easy task: fix bug

--- easy/script.js
+++ easy/script.js
@@ -41,7 +41,7 @@ console.log(`Дневной бюджет ${budgetDay} рублей`);
 
 if (budgetDay >= 1200) {
     console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 & budgetDay < 1200) {
+} else if (budgetDay >= 600 && budgetDay < 1200) {
     console.log('У вас средний уровень дохода');
 } else if (budgetDay < 600 && budgetDay >= 0) {
     console.log('К сожалению, у вас уровень дохода ниже среднего');
