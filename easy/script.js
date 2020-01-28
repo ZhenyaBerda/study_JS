e1041371364df26b2cbb912993453edac39b6e7b
unknown <bev030997@gmail.com>
2020-01-28 18:31:06 +0300

    easy task: done

--- easy/script.js
+++ easy/script.js
@@ -1,23 +1,59 @@
 'use strict';
 
+
 const mission = 100000, period = 4;
-let money, addExpenses, deposit, budgetMonth, budgetDay, expenses1, expenses2, amount1, amount2,
-income = 'стипендия'; 
+let money = prompt('Ваш месячный доход?', '60000'),
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую',
+    'Коммуналка, бензин, электроэнергия'),
+deposit = confirm('Есть ли у вас депозит в банке?'),
+expenses1 = prompt('Введите обязательную статью расходов?', 'Коммуналка'),
+amount1 = prompt('Во сколько это обойдется?', '2000'),
+expenses2 = prompt('Введите обязательную статью расходов?', 'Обучение'),
+amount2 = prompt('Во сколько это обойдется?', '2000'),
+income = 'стипендия';
 
-// Запросы пользователю
-money = prompt('Ваш месячный доход?');
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-deposit = confirm('Есть ли у вас депозит в банке?');
 
-expenses1 = prompt('Введите обязательную статью расходов?');
-amount1 = prompt('Во сколько это обойдется?');
-expenses2 = prompt('Введите обязательную статью расходов?');
-amount2 = prompt('Во сколько это обойдется?');
+// Объявления функций
+// Функция определения типа данных
+function showTypeOf (data) {
+    return typeof(data);
+}
+
+// Сумма обязательных расходов
+function getExpensesMonth (costs1, costs2) {
+    return costs1+costs2;
+}
+
+// Накопления за месяц
+function getAccumulatedMonth (gain, costs) {
+    return gain-costs;
+}
+
+// Функция расчета периода
+function getTargetMonth (budgetMonth) {
+    return Math.ceil(mission / budgetMonth);
+}
+
+// Функция определения статуса 
+function getStatusIncome (budget) {
+
+    if (budget >= 1200) {
+        return 'У вас высокий уровень дохода';
+    } else if (budget >= 600 && budget < 1200) {
+        return 'У вас средний уровень дохода';
+    } else if (budget < 600 && budget >= 0) {
+        return 'К сожалению, у вас уровень дохода ниже среднего';
+    } else if (budget < 0 ) {
+        return 'Что-то пошло не так';
+    }
+}
+
+
 
 // Вывод типов данных
-console.log(`Тип данных money: ${typeof money}`);
-console.log(`Тип данных income: ${typeof income}`);
-console.log(`Тип данных deposit: ${typeof deposit}`);
+console.log(`Тип данных money: ${showTypeOf(money)}`);
+console.log(`Тип данных income: ${showTypeOf(income)}`);
+console.log(`Тип данных deposit: ${showTypeOf(deposit)}`);
 
 // Вывод длины строки addExpenses
 console.log(`Длина строки addExpenses: ${addExpenses.length}`);
@@ -25,27 +61,20 @@ console.log(`Длина строки addExpenses: ${addExpenses.length}`);
 // Разбивка строки на массив
 console.log(addExpenses.toLowerCase().split(', '));
 
-// Вывод периода и цели
+// Вывод заданного периода и цели
 console.log(`Период равен ${period} месяца(ев) \nЦель заработать ${mission} рублей`);
 
-// Бюджет на месяц
-budgetMonth = Number(money - amount1 - amount2);
-console.log(`Бюджет на месяц: ${budgetMonth}`);
+// Расходы и месячные накопления
+const amounts = getExpensesMonth(Number(amount1), Number(amount2)),
+accumulatedMonth = getAccumulatedMonth(Number(money), amounts);
+console.log(`Сумма расходов: ${amounts}\nМесячные накопления: ${accumulatedMonth}`);
 
 // Вывод периода и цели
-console.log(`Цель будет достигнута за: ${Math.ceil(mission / budgetMonth)} месяцев`);
+console.log(`Цель будет достигнута за: ${getTargetMonth(accumulatedMonth)} месяца(ев)`);
 
 // Дневной бюджет
-budgetDay = Math.floor(budgetMonth / 30);
-console.log(`Дневной бюджет ${budgetDay} рублей`);
+const budgetDay = Math.floor(accumulatedMonth / 30);
+console.log(`Дневной бюджет ${budgetDay} рублей\n${getStatusIncome(budgetDay)}`);
 
-if (budgetDay >= 1200) {
-    console.log('У вас высокий уровень дохода');
-} else if (budgetDay >= 600 && budgetDay < 1200) {
-    console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600 && budgetDay >= 0) {
-    console.log('К сожалению, у вас уровень дохода ниже среднего');
-} else if (budgetDay < 0 ) {
-    console.log('Что-то пошло не так');
-}
+
 
