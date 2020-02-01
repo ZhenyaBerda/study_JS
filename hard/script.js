bd58f7d7d4ffaa966145acd075c4c71eed7eba02
unknown <bev030997@gmail.com>
2020-02-01 23:41:26 +0300

    hard task: code modified

--- hard/script.js
+++ hard/script.js
@@ -3,12 +3,14 @@
 // Задание 1
 const arr = [111111111, 22222222, 333333333, 444444444, 5555555, 666666666, 7777777777];
 
-arr.filter(function (item) {
+let filtArr = arr.filter(function (item) {
     item = item.toString();
     if (item.slice(0,1) === '2' || item.slice(0,1) === '4'){
-        console.log(item);
+        return item;
     }
 });
+
+console.log(filtArr);
 
 
 // Задание 2
