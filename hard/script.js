099342ccb25df82c458db68f38ba22371ec2c4ed
unknown <bev030997@gmail.com>
2020-02-01 23:59:07 +0300

    hard task: method modified

--- hard/script.js
+++ hard/script.js
@@ -1,20 +1,12 @@
 'use strict';
 
 // Задание 1
-const arr = [111111111, 22222222, 333333333, 444444444, 5555555, 666666666, 7777777777];
+const arr = ["111111111", "22222222", "333333333", "444444444", "5555555", "666666666", "7777777777"];
 
-let filtArr = arr.filter(function (item) {
-    item = item.toString();
-    if (item.slice(0,1) === '2' || item.slice(0,1) === '4'){
-        return item;
-    }
-});
-
-console.log(filtArr);
-
+const filtredArr = arr.filter( item => item[0] === '2' || item[0] === '4');
+console.log(filtredArr);
 
 // Задание 2
-
 outer: for (let i = 2; i <= 100; i++) {
     for (let j = 2; j < i; j++) {
         if (i % j === 0) {
