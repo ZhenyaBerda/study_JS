8824e0cbd1937df14ffe08ba3824dc48900e8266
unknown <bev030997@gmail.com>
2020-02-01 17:21:06 +0300

    hard task: fix output

--- hard/script.js
+++ hard/script.js
@@ -1,15 +1,22 @@
 'use strict';
 
-let week = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'],
+let week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
     date = new Date(),
     element = document.getElementById('text');
 
 week.forEach(function (item, i) {
-    if ((i === date.getDay() && i === 0) || (i === date.getDay() && i === 6)) {
+    let curDate;
+    if (date.getDay() !== 0) {
+        curDate = date.getDay() - 1;
+    } else {
+        curDate = 6;
+    }
+
+    if ((i === curDate && i === 5) || (i === curDate && i === 6)) {
         element.innerHTML += '<b><i>' + item + '</i></b><br>';
-    } else if (i === date.getDay()) {
+    } else if (i === curDate) {
         element.innerHTML += '<b>' + item + '</b><br>';
-    } else if (i === 0 || i === 6) {
+    } else if (i === 5 || i === 6) {
         element.innerHTML += '<i>' + item + '</i><br>';
     } else {
         element.innerHTML += item + '<br>';
