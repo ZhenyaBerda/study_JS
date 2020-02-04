e2d4b0729062a0858a715d511bdcc9cb01f7dc70
unknown <bev030997@gmail.com>
2020-02-05 00:18:38 +0300

    hard task: main functionality made

--- hard/script.js
+++ hard/script.js
@@ -1,27 +1,59 @@
 'use strict';
-const week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'],
+let curDate = new Date(),
     element = document.getElementById('text'),
-// получение текущего дня недели
+    // склонение
+    getDeclination = function (number, titles) {
+        let cases = [2, 0, 1, 1, 1, 2];
+        return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+    },
+    // день недели
     getCurDay = function () {
-        const date = new Date(),
-            days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-        
-            return (days[date.getDay()]);
+        let days = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+        return days[curDate.getDay()];
+    },
+    // месяц
+    getCurMonth = function () {
+        let month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+        return month[curDate.getMonth()];
+    },
+
+    // получение текущей даты и времени в текстовом виде
+    getTextDate = function () {
+        let day = `${curDate.getDate()} ${getCurMonth()} ${curDate.getFullYear()}`,
+            hour = curDate.getHours(),
+            minute = curDate.getMinutes(),
+            seconds = curDate.getSeconds(),
+            time = `${hour} ${getDeclination(hour, ['час', 'часа', 'часов'])}
+        ${minute} ${getDeclination(minute, ['минута', 'минуты', 'минут'])}
+        ${seconds} ${getDeclination(seconds, ['секунда', 'секунды', 'секунд'])}`;
+
+        return (`Сегодня ${getCurDay()}, ${day}, ${time}`);
+    },
+    // добавление нулей
+    addZero = function (number) {
+        let strNumber = number.toString();
+
+        if (strNumber.length === 1) {
+            return `0` + strNumber;
+        } else {
+            return strNumber;
+        }
+    },
+    // сокращенная версия
+    getShortDate = function () {
+        let day = addZero(curDate.getDate()),
+            month = addZero(curDate.getMonth() + 1),
+            hour = addZero(curDate.getHours()),
+            minute = addZero(curDate.getMinutes()),
+            seconds = addZero(curDate.getSeconds());
+
+        return `${day}.${month}.${curDate.getFullYear()} - ${hour}:${minute}:${seconds}'`;
+    },
+    // вывод
+
+    output = function () {
+    document.getElementById('text-time').textContent = getTextDate();
+    document.getElementById('short').textContent = getShortDate();
     };
 
-
-week.forEach(function (item) {
-    
-   let p = document.createElement('p');
-
-    if (item === 'saturday' || item === 'sunday') {
-        p.classList.add('bold');
-    }
-    if (item === getCurDay()) {
-        p.classList.add('italic');
-    }
-
-    p.textContent += item;
-    document.body.appendChild(p);
-
-});
+    setInterval (output, 1000);
