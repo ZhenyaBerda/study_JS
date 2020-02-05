8ca8769ad56194754bc428e5409ed813b9a444fc
unknown <bev030997@gmail.com>
2020-02-05 08:07:58 +0300

    hard task: fix bugs

--- hard/script.js
+++ hard/script.js
@@ -1,25 +1,25 @@
 'use strict';
-let curDate = new Date(),
-    element = document.getElementById('text'),
+let element = document.getElementById('text'),
     // склонение
     getDeclination = function (number, titles) {
         let cases = [2, 0, 1, 1, 1, 2];
         return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
     },
     // день недели
-    getCurDay = function () {
+    getCurDay = function (curDate) {
         let days = ['Воскресение', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
         return days[curDate.getDay()];
     },
     // месяц
-    getCurMonth = function () {
+    getCurMonth = function (curDate) {
         let month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
         return month[curDate.getMonth()];
     },
 
     // получение текущей даты и времени в текстовом виде
     getTextDate = function () {
-        let day = `${curDate.getDate()} ${getCurMonth()} ${curDate.getFullYear()}`,
+        let curDate = new Date();
+        let day = `${curDate.getDate()} ${getCurMonth(curDate)} ${curDate.getFullYear()}`,
             hour = curDate.getHours(),
             minute = curDate.getMinutes(),
             seconds = curDate.getSeconds(),
@@ -27,7 +27,7 @@ let curDate = new Date(),
         ${minute} ${getDeclination(minute, ['минута', 'минуты', 'минут'])}
         ${seconds} ${getDeclination(seconds, ['секунда', 'секунды', 'секунд'])}`;
 
-        return (`Сегодня ${getCurDay()}, ${day}, ${time}`);
+        return (`Сегодня ${getCurDay(curDate)}, ${day}, ${time}`);
     },
     // добавление нулей
     addZero = function (number) {
@@ -41,6 +41,7 @@ let curDate = new Date(),
     },
     // сокращенная версия
     getShortDate = function () {
+        let curDate = new Date();
         let day = addZero(curDate.getDate()),
             month = addZero(curDate.getMonth() + 1),
             hour = addZero(curDate.getHours()),
@@ -52,8 +53,8 @@ let curDate = new Date(),
     // вывод
 
     output = function () {
-    document.getElementById('text-time').textContent = getTextDate();
-    document.getElementById('short').textContent = getShortDate();
+        document.getElementById('text-time').textContent = getTextDate();
+        document.getElementById('short').textContent = getShortDate();
     };
 
-    setInterval (output, 1000);
+setInterval(output, 1000);
